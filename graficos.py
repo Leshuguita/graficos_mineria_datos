b1d1f955d11ea4ed85a217ddeb236c1e7a6ad4fd
Leshuguita <69051056+Leshuguita@users.noreply.github.com>
2024-09-12 11:15:36 -0300

    comentar codigo

--- graficos.py
+++ graficos.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
 print("Cargando transacciones...")
-# full file
+# archivo completo
 tdf = pd.read_csv("./credit_card_transactions-ibm_v2.csv")
-# user 0 only (faster)
+# solo user 0 (mas rapido, para probar cosas)
 #tdf = pd.read_csv("./User0_credit_card_transactions.csv")
 print("Cargando tarjetas...")
 cdf = pd.read_csv("./sd254_cards.xls")
@@ -25,18 +25,23 @@ joined = cdf.rename(columns={"CARD INDEX":"Card"}).join(fraud_per_card, on=["Use
 
 print("Dibujando gráficos...")
 
+# grafico de torta de promedio de fraudes
+# segun tipo de tarjeta
 mean = joined[["Card Type", "Fraud Count"]].groupby("Card Type").mean()
 plot = mean.plot.pie(y="Fraud Count", legend=False, autopct="%1.1f%%",ylabel="", title="Promedio de fraudes por tipo de tarjeta")
 fig = plot.get_figure()
 fig.savefig("./by_card_type.svg")
 
-# no se si esto aporta mucho mas que el de torta la verdad
+# lo mismo que antes pero boxplots,
+# no aporta mucho mas que el de torta la verdad
 group = joined[["Card Type", "Fraud Count"]].groupby("Card Type")
 by_type = pd.concat([pd.Series(v['Fraud Count'].tolist(), name=k) for k, v in group], axis=1)
 plot = by_type.plot.box(vert=False)
 fig = plot.get_figure()
 fig.savefig("./by_card_type_box.svg")
 
+# promedio de frauded segun cantidad de copias
+# de la tarjeta
 plot = joined[["Cards Issued", "Fraud Count"]].groupby("Cards Issued").mean().plot.bar(title="Promedio de fraudes por cantidad de copias de tarjeta")
 fig = plot.get_figure()
 fig.savefig("./by_num_copies.svg")
@@ -46,16 +51,19 @@ fig.savefig("./by_num_copies.svg")
 # fig = plot.get_figure()
 # fig.savefig("./by_dark_web.svg")
 
+# histograma de cantitad de fraudes
+# segun ultimo cambio de clave
 last_changed = joined[["Year PIN last Changed", "Fraud Count"]]
-# para el repeat me reclama por los ceros, y para el histogrma da lo mismo
 last_changed = last_changed.loc[last_changed["Fraud Count"]!=0]
 last_changed = last_changed.loc[last_changed.index.repeat(last_changed["Fraud Count"].max(0))]
 plot = last_changed.plot.hist(column="Year PIN last Changed", bins=18, title="Cantidad de fraudes por año de última renovacion")
 fig = plot.get_figure()
 fig.savefig("./by_pin_change.svg")
+# *podria ser interesante filtrar segun si la transaccion
+# se hizo antes o despues del cambio, pero no tuve tiempo
 
-plot = joined.plot.hist(column="Year PIN last Changed", bins=18, title="Cantidad de última renovacion por año")
-fig = plot.get_figure()
-fig.savefig("./renew_by_year.svg")
+#plot = joined.plot.hist(column="Year PIN last Changed", bins=18, title="Cantidad de última renovacion por año")
+#fig = plot.get_figure()
+#fig.savefig("./renew_by_year.svg")
 
 print("Listo! :)")
